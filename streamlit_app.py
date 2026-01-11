da8f2b4d42e263100f9b9b2156efad35635af02d
alexsvetlit-wq <alexsvetlit@icloud.com>
2026-01-11 15:42:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ col1, col2 = st.columns([1, 5])
 with col1:
     st.image("assets/bris_logo.png", width=120)
 with col2:
-    st.title("BRIS Logistics калькулятор")
+    st.title("Logistics калькулятор")
 VAT_PCT_FIXED = 22.0
 
 # =========================
